8b374904e30ab48b84feaa46a1d54de41428feed
Santhosh Kumar Kaliappan <santosh.k.kaliappan@pwc.com>
2017-07-26 16:53:53 +0530

    Added Bootstrap Module and Routing, Templating update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 platformBrowserDynamic().bootstrapModule(AppModule);
